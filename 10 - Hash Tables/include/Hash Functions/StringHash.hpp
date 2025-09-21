eb02391167f818e1f4f0b837d66ac26842896a81
ascendho <coderhs@foxmail.com>
2025-09-21 21:29:23 +0800

    constexpr与const区别注释

--- 10 - Hash Tables/include/Hash Functions/StringHash.hpp
+++ 10 - Hash Tables/include/Hash Functions/StringHash.hpp
@@ -2,14 +2,19 @@
 
 #include <string>
 
-// constexpr 与 const
+/*
+    constexpr 类型的函数通常需要在头文件中定义，编译时求值需要
+    constexpr 在编译时求值，零运行时开销
+    const 在运行时求值，可能有开销
+*/
+
 // 质数因子
 constexpr size_t CONST = 31;
 
-// constexpr 类型的函数通常需要在头文件中定义，编译时求值需要
 constexpr size_t characterToNumber(char c) noexcept {
     // 类型转换操作符，常用&安全
     return static_cast<size_t>(c);
 }
 
+// StringHash 涉及动态内存分配以及参数 std::string 不是字面量类型，无法在编译时求值
 size_t StringHash(const std::string &key, size_t size) noexcept;
