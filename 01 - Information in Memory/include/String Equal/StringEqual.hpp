3b178e3760036a595d51da4b73ebbd2472136a0b
Ascend Ho <coderhs@foxmail.com>
2025-09-25 17:31:50 +0800

    使用 std::string_view

--- 01 - Information in Memory/include/String Equal/StringEqual.hpp
+++ 01 - Information in Memory/include/String Equal/StringEqual.hpp
@@ -1,4 +1,16 @@
 #pragma once
-#include <string>
+#include <string_view>
 
-bool StringEqual(const std::string& str1, const std::string& str2);
+/*
+ * 非拥有（non-owning）的字符串视图：
+ * 1. 只是对现有字符串数据的引用
+ * 2. 持有指向字符串数据的指针和字符串长度
+ * 3. 不管理（不分配、不复制、不释放）字符串内存
+ * 4. 只能读取，不能修改底层数据
+ * 5. 零拷贝，非常轻量，开销成本极低
+ *
+ */
+
+ // string_view 本身就是只读的，不需要额外的 const 保护
+ // 同时字符串视图是轻量级对象，按值传递成本低，也更容易被编译器优化
+bool StringEqual(std::string_view str1, std::string_view str2);
--- 01 - Information in Memory/src/String Equal/StringEqual.cpp
+++ 01 - Information in Memory/src/String Equal/StringEqual.cpp
@@ -1,11 +1,11 @@
 #include "../../include/String Equal/StringEqual.hpp"
 
-bool StringEqual(const std::string &str1, const std::string &str2) {
+bool StringEqual(std::string_view str1, std::string_view str2) {
     if (str1.size() != str2.size())
         return false;
 
     size_t N = str1.size();
-    size_t i = 0;
+    int i = 0;
 
     while (i < N && str1[i] == str2[i])
         i = i + 1;
